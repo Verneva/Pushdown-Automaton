fede482b65149e93b5dd3d5da295173b70f2dba3
Verneva <verneva.ziga@gmail.com>
2025-10-26 15:37:52 -0500

    Rename stack to stk
    Rename stack to stk to fix compilation error

--- PDA.cpp
+++ PDA.cpp
@@ -3,24 +3,23 @@
 #include <stack>
 using namespace std;
 
-
 //Create PDA class
 class PDA{
 
 private:
     enum State {Start, alpha, blank, openParen, closedParen, openBrack, closedBrack, retT, retF};
-//Define a private stack so that it isn't continuously created when processChar() is called
-    stack<string> stack;
+    //Rename stack variable to avoid naming conflict with stack
+    stack<string> stk;
 
-//Use switch cases to move the states for each character
-public:
+public: 
     State currentState = Start;
+
     bool ProcessChar(char c) {
-    //Only add $ if the stack is new and empty
-        if (stack.empty() == true) {
-            stack.push("$");
-        };
-    
+        //Only add $ if the stack is new and empty
+        if (stk.empty()) {
+            stk.push("$");
+        }
+
         switch (currentState){
             case Start:
                 if (c >= 'a' && c <= 'z'){
@@ -28,63 +27,65 @@ public:
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                }  else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                }  else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
-                } else if (c == '.' && stack.top() == "$"){
-                    stack.pop();
+                } else if (c == '.' && stk.top() == "$"){
+                    stk.pop();
                     currentState = retT;
                 } else{
                     currentState = retF;
                 }
                 break;
+
             case alpha:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "[") {
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "[") {
+                    stk.pop();
                     currentState = closedBrack;
-                } else if (c == '.' && stack.top()=="$"){
+                } else if (c == '.' && stk.top() == "$"){
                     currentState = retT;
                 } else{
                     currentState = retF;
                 }
                 break;
+
             case blank:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
                 } else if (c == '.'){
                     currentState = retT;
@@ -92,22 +93,23 @@ public:
                     currentState = retF;
                 }
                 break;
+
             case openParen:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "("){
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "("){
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
                 } else if (c == '.'){
                     currentState = retF;
@@ -115,46 +117,48 @@ public:
                     currentState = retF;
                 }
                 break;
+
             case closedParen:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
-                } else if (c == '.' && stack.top() == "$"){
-                    stack.pop();
+                } else if (c == '.' && stk.top() == "$"){
+                    stk.pop();
                     currentState = retT;
                 } else{
                     currentState = retF;
                 }
                 break;
+
             case openBrack:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
                 } else if (c == '.'){
                     currentState = retF;
@@ -162,60 +166,56 @@ public:
                     currentState = retF;
                 }
                 break;
+
             case closedBrack:
                 if (c >= 'a' && c <= 'z'){
                     currentState = alpha;
                 } else if (c == ' '){
                     currentState = blank;
                 } else if (c == '('){
-                    stack.push("(");
+                    stk.push("(");
                     currentState = openParen;
-                } else if (c == ')' && stack.top() == "(") {
-                    stack.pop();
+                } else if (c == ')' && stk.top() == "(") {
+                    stk.pop();
                     currentState = closedParen;
                 } else if (c == '['){
-                    stack.push("[");
+                    stk.push("[");
                     currentState = openBrack;
-                } else if (c == ']' && stack.top() == "["){
-                    stack.pop();
+                } else if (c == ']' && stk.top() == "["){
+                    stk.pop();
                     currentState = closedBrack;
                 } else if (c == '.'){
                     currentState = retT;
                 } else{
                     currentState = retF;
                 }
-                break;
+                break;       
         }
         return (currentState == retT) || (currentState == retF);
-
-     };
+    }
 
     bool IsAccepted() const {
         return currentState == retT;
     }
-
-
 };
 
 int main(){
-//Create PDA object
+    //Create PDA object
     PDA pda;
     
     string input;
 
     cout <<"Enter a string (Σ = { a-z, ␢, (, ), [, ] }) followed by '.' ";
-    // getline() allows program to read multiple blank spaces
     getline(cin, input);
 
-//Loop through each character and call processChar()
+    //Loop through each character and call ProcessChar()
     for (char c : input)
-    if (pda.ProcessChar(c)){
-        if (pda.IsAccepted())
-            cout << "String accepted!" << endl;
-        else
-            cout << "String not accepted! " << endl;
-        break;
-    }
+        if (pda.ProcessChar(c)){
+            if (pda.IsAccepted())
+                cout << "String accepted!" << endl;
+            else
+                cout << "String not accepted!" << endl;
+            break;
+        }
     return 0;
-};
-
+}
